46b0242bd0be8ed165b8c75369180b4aa4f88a1c
Nobuo1106 <gonnbe1986@gmail.com>
2023-07-26 22:11:35 +0900

    Toastで選択したアイテムを表示

--- app/src/main/java/com/websarva/wings/android/listlistnersample/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/listlistnersample/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.websarva.wings.android.listlistnersample
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.text.FieldPosition
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val lvMenu = findViewById<ListView>(R.id.lvMenu)
+        lvMenu.onItemClickListener = ListItemClickListner()
+    }
+
+    private inner class ListItemClickListner : AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+            val item = parent.getItemAtPosition(position) as String
+            val show = "あなたが選んだ定食" + item
+            Toast.makeText(this@MainActivity, show, Toast.LENGTH_LONG).show()
+        }
     }
 }
