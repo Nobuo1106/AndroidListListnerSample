108d60d7103a0c91b01be27a4ee867ecc6dfdf2a
Nobuo1106 <gonnbe1986@gmail.com>
2023-08-11 13:07:39 +0900

    done

--- app/src/main/java/com/websarva/wings/android/listlistnersample/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/listlistnersample/MainActivity.kt
@@ -14,14 +14,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val lvMenu = findViewById<ListView>(R.id.lvMenu)
-        lvMenu.onItemClickListener = ListItemClickListner()
+        lvMenu.onItemClickListener = ListItemClickListener()
     }
 
-    private inner class ListItemClickListner : AdapterView.OnItemClickListener {
+    private inner class ListItemClickListener : AdapterView.OnItemClickListener {
         override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-            val item = parent.getItemAtPosition(position) as String
-            val show = "あなたが選んだ定食" + item
-            Toast.makeText(this@MainActivity, show, Toast.LENGTH_LONG).show()
+            val dialogFragment = OrderConfirmDialogFragment()
+            dialogFragment.show(supportFragmentManager, "OrderConfirmDialogFragment")
         }
     }
 }
